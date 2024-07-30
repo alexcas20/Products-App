0597c35bf30fb44212f8af8b05f30a7b7fd63587
alexcas20 <alexcalvillosalas@gmail.com>
2024-07-29 22:24:30 -0600

    Function to simulate connection with db and get data

---
+++ src/helpers/getData.js
@@ -0,0 +1,12 @@
+import data from "../data/data.json";
+
+
+export const getData = () => {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            resolve(data);
+        }, 500)
+     
+    })
+}
